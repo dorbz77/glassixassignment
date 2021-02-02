26b6a49c7d4b43eca71950b9fda043442eb97a19
Dor Ben Zaken <dorbenzaken@gmail.com>
2021-02-02 13:49:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [ipData, setIpData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+        // Get IP data
+        let userIpDataObj = await axios.get('https://api.ipdata.co/?api-key=dcb19aafdd3accd0ca61b014f91a6cc5888d57f4f703a9758da3751e');
+        userIpDataObj = userIpDataObj.data;
+        setIpData(userIpDataObj);
+    })();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="locationDetails">
+        City: {ipData.city}, Country: {ipData.country_name}
+      </div>
+      <iframe
+      width="600"
+      height="450"
+      frameBorder="0" style={{ border: 0 }}
+      src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCdQymwSuF0P6Ee-ffX0ZtWjpJdpaT5eLk
+        &q=${ipData.city},${ipData.country_name}`} allowFullScreen>
+      </iframe>
     </div>
   );
 }
